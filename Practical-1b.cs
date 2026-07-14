cdb2d676f94d204ef29c3cacd443f45cd605b662
sachmehtmvlu-oss <sachmehtmvlu@gmail.com>
2026-07-14 09:37:54 +0530

    Add files via upload

---
+++ Practical-1b.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Floyd
+{
+    static void Main(string[] args)
+    {
+        int count = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(count + " ");
+                count++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Sachin Ramesh Mehta");
+    }
+}
