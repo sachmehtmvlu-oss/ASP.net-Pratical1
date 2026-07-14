69ad59288756106d6e8dd14c77f322522c7940a5
sachmehtmvlu-oss <sachmehtmvlu@gmail.com>
2026-07-14 09:40:09 +0530

    Add files via upload

---
+++ Practical-1c.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class fylod
+{
+    static void Main()
+    {
+        string[] city = new string[5];
+        Console.WriteLine("Sachin Ramesh Mehta");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter city name: ");
+            city[i] = Console.ReadLine();
+        }
+        city.Sort();
+        Console.WriteLine("------------sorted-------------");
+        for (int i = 0; i < city.Length; i++)
+        {
+            Console.WriteLine(city[i]);
+        }
+    }
+}
